4bd3335baf102a0314db41f5e010c90caf5a2d25
Zaahiid <zahidrsheikh01@gmail.com>
2025-03-24 19:11:10 +0530

    Added Main layout

---
+++ src/routes/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import LeftBar from "../../components/leftBar/leftBar";
+import TopBar from "../../components/topBar/topBar";
+import { Outlet } from "react-router";
+import "./MainLayout.css";
+
+const MainLayout = () => {
+  return (
+    <div className="app">
+      <LeftBar />
+      <div className="content">
+        <TopBar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
